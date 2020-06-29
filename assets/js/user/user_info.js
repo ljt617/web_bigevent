0ab6cb8de40ae27859d0db5b074b44896f493a27
lijiangtao <1214422633@qq.com>
2020-06-29 11:29:54 +0800

    完成了个人中心功能的分支

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,57 +1,109 @@
-$(function() {
-  var form = layui.form
-  var layer = layui.layer
-
+$(function () {
+  var form = layui.form;
+  var layer = layui.layer;
   form.verify({
-    nickname: function(value) {
-      if (value.length > 6) {
-        return '昵称长度必须在 1 ~ 6 个字符之间！'
+    nickname: function (value) {
+      if (value > 6) {
+        return `昵称长度不得大于6个字符`;
       }
-    }
-  })
+    },
+  });
 
-  initUserInfo()
+  // form.verify({
+  //   nickname: function(value) {
+  //     if (value.length > 6) {
+  //       return '昵称长度必须在 1 ~ 6 个字符之间！'
+  //     }
+  //   }
+  // })
+
+  initUserInfo();
 
   // 初始化用户的基本信息
   function initUserInfo() {
     $.ajax({
-      method: 'GET',
-      url: '/my/userinfo',
-      success: function(res) {
+      method: "GET",
+      url: "/my/userinfo",
+      success: function (res) {
         if (res.status !== 0) {
-          return layer.msg('获取用户信息失败！')
+          return layer.msg(res.message);
         }
-        // console.log(res)
-        // 调用 form.val() 快速为表单赋值
-        form.val('formUserInfo', res.data)
-      }
-    })
+        //调用layui'中的表单方法 form.val快速为表单赋值
+        form.val("formUserInfo", res.data);
+        console.log(res.data);
+      },
+    });
   }
 
+  // function initUserInfo() {
+  //   $.ajax({
+  //     method: "GET",
+  //     url: "/my/userinfo",
+  //     success: function (res) {
+  //       if (res.status !== 0) {
+  //         return layer.msg("获取用户信息失败！");
+  //       }
+  //       // console.log(res)
+  //       // 调用 form.val() 快速为表单赋值
+  //       form.val("formUserInfo", res.data);
+  //     },
+  //   });
+  // }
+
   // 重置表单的数据
-  $('#btnReset').on('click', function(e) {
-    // 阻止表单的默认重置行为
-    e.preventDefault()
-    initUserInfo()
-  })
+  $("#btnReset").on("click", function (e) {
+    //阻止默认的重置行为
+    e.preventDefault();
+    //点击重置按钮后让表单重新获取数据赋值
+    initUserInfo();
+  });
+
+  // $("#btnReset").on("click", function (e) {
+  //   // 阻止表单的默认重置行为
+  //   e.preventDefault();
+  //   initUserInfo();
+  // });
 
   // 监听表单的提交事件
-  $('.layui-form').on('submit', function(e) {
-    // 阻止表单的默认提交行为
-    e.preventDefault()
-    // 发起 ajax 数据请求
+  $(".layui-form").on("submit", function (e) {
+    //阻止表单的默认提交行为
+    e.preventDefault();
+    // let inputParams = form.val("formUserInfo");
+    // delete inputParams.username;
+    //发送ajax请求
     $.ajax({
-      method: 'POST',
-      url: '/my/userinfo',
+      method: "POST",
+      url: "/my/userinfo",
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
-          return layer.msg('更新用户信息失败！')
+          return layer.msg(res.message);
         }
-        layer.msg('更新用户信息成功！')
-        // 调用父页面中的方法，重新渲染用户的头像和用户的信息
-        window.parent.getUserInfo()
-      }
-    })
-  })
-})
+        layer.msg(res.message);
+        
+
+        //调用父页面中的方法重新渲染用户头像和信息
+        window.parent.getUserInfo();
+      },
+    });
+  });
+
+  // $(".layui-form").on("submit", function (e) {
+  //   // 阻止表单的默认提交行为
+  //   e.preventDefault();
+  //   // 发起 ajax 数据请求
+  //   $.ajax({
+  //     method: "POST",
+  //     url: "/my/userinfo",
+  //     data: $(this).serialize(),
+  //     success: function (res) {
+  //       if (res.status !== 0) {
+  //         return layer.msg("更新用户信息失败！");
+  //       }
+  //       layer.msg("更新用户信息成功！");
+  //       // 调用父页面中的方法，重新渲染用户的头像和用户的信息
+  //       window.parent.getUserInfo();
+  //     },
+  //   });
+  // });
+});
--- assets/js/user/user_pwd.js
+++ assets/js/user/user_pwd.js
@@ -1,34 +1,73 @@
-$(function() {
-  var form = layui.form
-
+$(function () {
+  //定义from模块
+  var form = layui.form;
+  var layer = layui.layer;
   form.verify({
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    samePwd: function(value) {
-      if (value === $('[name=oldPwd]').val()) {
-        return '新旧密码不能相同！'
+    pwd: [/^[\S]{6,12}$/, "密码必须6到12位，且不能出现空格"],
+    samePwd: function (value) {
+      if (value === $('input[name="oldPwd"]').val()) {
+        return `与原密码相同请更换`;
       }
     },
-    rePwd: function(value) {
-      if (value !== $('[name=newPwd]').val()) {
-        return '两次密码不一致！'
+    rePwd: function (value) {
+      if (value !== $('input[name="newPwd"]').val()) {
+        return `两次密码不一致请重新输入`;
       }
-    }
-  })
+    },
+  });
 
-  $('.layui-form').on('submit', function(e) {
-    e.preventDefault()
+  // 设置ajax请求
+  $(".layui-form").on("submit", function (e) {
+    //阻止默认提交行为
+    e.preventDefault();
+    //发起ajax请求
     $.ajax({
-      method: 'POST',
-      url: '/my/updatepwd',
-      data: $(this).serialize(),
-      success: function(res) {
+      method: "POST",
+      url: "/my/updatepwd",
+      data: {
+        oldPwd: $('[name="oldPwd"]').val(),
+        newPwd: $('[name="newPwd"]').val(),
+      },
+      success: function (res) {
         if (res.status !== 0) {
-          return layui.layer.msg('更新密码失败！')
+          return layer.msg(res.message);
         }
-        layui.layer.msg('更新密码成功！')
-        // 重置表单
-        $('.layui-form')[0].reset()
-      }
-    })
-  })
-})
+
+        layer.msg(res.message);
+        //重置表单用dom对象方法
+        $(".layui-form")[0].reset();
+      },
+    });
+  });
+
+  // form.verify({
+  //   pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+  //   samePwd: function(value) {
+  //     if (value === $('[name=oldPwd]').val()) {
+  //       return '新旧密码不能相同！'
+  //     }
+  //   },
+  //   rePwd: function(value) {
+  //     if (value !== $('[name=newPwd]').val()) {
+  //       return '两次密码不一致！'
+  //     }
+  //   }
+  // })
+
+  // $(".layui-form").on("submit", function (e) {
+  //   e.preventDefault();
+  //   $.ajax({
+  //     method: "POST",
+  //     url: "/my/updatepwd",
+  //     data: $(this).serialize(),
+  //     success: function (res) {
+  //       if (res.status !== 0) {
+  //         return layui.layer.msg("更新密码失败！");
+  //       }
+  //       layui.layer.msg("更新密码成功！");
+  //       // 重置表单
+  //       $(".layui-form")[0].reset();
+  //     },
+  //   });
+  // });
+});
